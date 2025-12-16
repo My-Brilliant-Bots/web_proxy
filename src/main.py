551008209eedbd3bce4b088075331afcd17e2fcb
Dennis Chacko <Dennis.Chacko@SignetJewelers.com>
2025-12-16 15:06:12 -0500

    refactored proxyAgent

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from tools import get_latest_traffic_log_file, read_file_content
 from proxyAgent import run_proxy_agent
 import asyncio
 
+
 async def main():
     dotenv_path = find_dotenv()
     load_dotenv(dotenv_path, override=True)
@@ -14,7 +15,9 @@ async def main():
     latest_log = get_latest_traffic_log_file(os.getenv("LOGS_DIRECTORY"))
     print(f"Latest traffic log file: {latest_log}")
     file_content = read_file_content(latest_log)
-    await run_proxy_agent(file_content)
+    consolidated_report = await run_proxy_agent(file_content)
+    print(consolidated_report)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- src/proxyAgent.py
+++ src/proxyAgent.py
@@ -5,13 +5,9 @@ from autogen_core.models import ModelInfo
 from autogen_core import CancellationToken
 import markdown
 import asyncio
-
 import os
 
-async def run_proxy_agent(traffic_log_file_content):
-    model_client = OpenAIChatCompletionClient(
-    model="gpt-4.1-nano",
-    model_info=ModelInfo(vision=True, function_calling=True, json_output=True, family="unknown", structured_output=True),)
+async def web_traffic_analysis(model_client, traffic_log_file_content):
 
     system_prompt = """
     You are a proxy agent that can help with web traffic analysis. You are given the contents of a traffic log file and you need to analyze the traffic and provide a report. The report should be in markdown format. The report should be in the following sections: 1. Overview 2. List of all urls visited and the number of times they were visited. Respond with the report only, no other text or comments. Ignore requsts for images, icons or JS. Ignore any bacground tracking requests or ad requests that a website may make. Only consider core requests pertinent to what the user is actually typing in the browser
@@ -21,26 +17,22 @@ async def run_proxy_agent(traffic_log_file_content):
     messages = []
     for i, chunk in enumerate(traffic_log_file_content):
         message_content = f"Here is chunk {i+1} of the traffic log file:\n{chunk}"
-        print(message_content)
         messages.append(TextMessage(content=message_content, source="user"))
 
-    
-
-    print(len(messages))
     all_agent_responses = []
     for idx, a_message in enumerate(messages):
+        if (idx > 1):
+          print("Warning : Not sending all file data to speed up testing. Remove this check when testing is complete")
+          break;
         agent:AssistantAgent = AssistantAgent(name="proxy_agent",model_client=model_client)
         response: TextMessage = await agent.on_messages(messages=[system_message,a_message], cancellation_token=CancellationToken())
-        agent_response = markdown.markdown(response.chat_message.content)
         all_agent_responses.append(response)
-        
-
-        print(agent_response)
-        print(f"Request# {idx}")
 
         await asyncio.sleep(70)
+    return all_agent_responses
 
-    
+async def create_consolidate_report(model_client, all_agent_responses):
+
     consolidate_report_prompt="""
     You are a smart agent responsible for merging multiple, markdown reports into a single consolidated
     report. Each individual markdown report, that you have to consolidate, contains an user's requests.These requests are captured using a proxy server. The consolidated report, should ignore requests to fetch javascript files, icons, images. It should ignore calls for health checks or requests that track an user. The report should provide a clear picture of the user's browsing behaviour but should not contain information of all networks calls to fetch images, javascript files etc. Finally , you should create the report in markdown language
@@ -54,7 +46,16 @@ async def run_proxy_agent(traffic_log_file_content):
     agent:AssistantAgent = AssistantAgent(name="consolidate_report_agent",model_client=model_client,system_message=consolidate_report_prompt)
     response: TextMessage = await agent.on_messages(messages=reports, cancellation_token=CancellationToken())
     consolidate_report = markdown.markdown(response.chat_message.content)
-    print(consolidate_report)
+    return consolidate_report
+
+async def run_proxy_agent(traffic_log_file_content):
+    model_client = OpenAIChatCompletionClient(
+    model="gpt-4.1-nano",
+    model_info=ModelInfo(vision=True, function_calling=True, json_output=True, family="unknown", structured_output=True),)
+
+    all_agent_responses = await web_traffic_analysis(model_client, traffic_log_file_content)
+    consolidated_report = await create_consolidate_report(model_client, all_agent_responses)
+    return consolidated_report
 
     
         
