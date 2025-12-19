6219ccb712bdeb64c1a53c7cfc3f2522969d64ec
Dennis Chacko <Dennis.Chacko@SignetJewelers.com>
2025-12-19 06:24:37 -0500

    work in progress

--- run.sh
+++
@@ -1,5 +0,0 @@
-export REQUESTS_CA_BUNDLE=/Users/dennis_chacko/Documents/GitHub/web_proxy/mitmproxy-ca-cert.pem
-export NO_PROXY=api.openai.com,openai.com
-export no_proxy=api.openai.com,openai.com
-
-/Users/dennis_chacko/Documents/GitHub/web_proxy/.venv/bin/python /Users/dennis_chacko/Documents/GitHub/web_proxy/src/main.py
